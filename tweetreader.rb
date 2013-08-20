343900845baa49fb22d2a3450b6f27931ebc7d83
Ed Jefferson <e.jefferson@gmail.com>
2013-08-20 14:56:44 +0100

    using right file now

--- tweetreader.rb
+++ tweetreader.rb
@@ -29,13 +29,22 @@ end
 
 
 def read_tweet(status)
-  if status.in_reply_to_user_id.to_i == 1678701920 && status.user.id != 1678701920 && status.id > Tweet.maximum(:id)
+  
+  puts status.urls.inspect
+  if status.text.match(/^@partyprinter.*/) && status.user.id != 1678701920 && Tweet.exists?(status.id.to_i) == nil
     image_urls = []
     status.media.each do |media|
       if image_check(media.media_url)
         image_urls << media.media_url
       end
     end
+
+    status.urls.each do |url|
+      if image_check(url.expanded_url)
+        image_urls << url.expanded_url
+      end
+    end
+
     puts "reading tweet #{status.text}"
     Tweet.create(:id => status.id.to_s, :text => status.text.gsub(/^@partyprinter /,""), :name => status.user.name, :screen_name => status.user.screen_name, :created_at => status.created_at, :images => image_urls, :printed => "0")
     
@@ -68,6 +77,16 @@ puts "checking stream"
 
 twitter_authorisation(TweetStream)
 client = TweetStream::Client.new
+
+client.on_error do |message|
+  # Log your error message somewhere
+  puts "ERROR: #{message}"
+end
+
+client.on_limit do |skip_count|
+  # do something
+  puts "RATE LIMITED LOL"
+end
 
 client.userstream do |status|
   read_tweet(status)
