172d65a30b8c03bcdf2dfbae0af24e339e935a40
Ed Jefferson <e.jefferson@gmail.com>
2013-08-21 20:19:34 +0100

    can't to_s time?

--- queue.rb
+++ queue.rb
@@ -13,7 +13,7 @@ require 'time'
 
 class Tweet < ActiveRecord::Base
   def localtime(time)
-    Time.parse(time).getlocal.to_s
+    Time.parse(time).getlocal.strftime "%Y-%m-%d %H:%M:%S %z"
   end
 
   def print
