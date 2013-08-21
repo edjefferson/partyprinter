c22b520ab7874ee36e3b21678fb313e12c032003
Ed Jefferson <e.jefferson@gmail.com>
2013-08-21 17:50:42 +0100

    merging changes

---
+++ tubestatus.rb
@@ -0,0 +1,27 @@
+class Tubestatus < ActiveRecord::Base
+
+  def process(tweet)
+    statuses = []
+    tubestatus = Tube::Status.new
+    tubestatus.lines.each {|line| statuses << [extend_line_name(line.name), line.status]}
+    Tubestatus.create(:id => status.id.to_s, :statuses => statuses)
+  end
+
+  def print(item)
+    puts created_at
+    @printer.set_underline_on
+    @printer.print_line "Tube status at #{created_at}"
+    @printer.set_underline_off
+
+    statuses.each do |linestatus|
+      @printer.set_font_weight_bold
+      @printer.print "#{linestatus[0]}:"
+      @printer.set_font_weight_normal
+      @printer.print " #{linestatus[1]}\n"
+    end
+
+    @printer.feed_and_cut
+    Tubestatus.destroy(tweet_id)
+  end
+
+end
