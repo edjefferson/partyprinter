7fe56477ef83b3493244ce11abb990a38443ddf7
Ed Jefferson <e.jefferson@gmail.com>
2013-08-23 11:20:38 +0100

    separating checking bugger and printing

--- lib/microprinter.rb
+++ lib/microprinter.rb
@@ -4,7 +4,7 @@ require 'pg'
 class Microprinter
 
   def initialize(port_str = "/dev/ttyACM0")
-    @pg.conn = (ENV['DATABASE'])
+    #@pg.conn = (ENV['DATABASE'])
     @port_str = port_str 
     baud_rate = 9600
     data_bits = 8
@@ -18,21 +18,22 @@ class Microprinter
     unprinted_sequences = pg.query "select * from buffer"
 
     unprinted_sequences.each do |sequence|
-      sequence.each do |instruction|
-
-        if instruction == "BS"
-          sleep 0.01
-        elsif instruction == "LS"
-          sleep 0.002
-        elsif instruction == "FL"
-          sp.flush
-        else
-          sp.putc instruction
-        end
-        
-      end
-
+      print(sequence)
       pg.query "delete from buffer"
+    end
+  end
+  
+  def print(sequence)
+    sequence.each do |instruction|
+      if instruction == "BS"
+        sleep 0.01
+      elsif instruction == "LS"
+        sleep 0.002
+      elsif instruction == "FL"
+        sp.flush
+      else
+        sp.putc instruction
+      end
     end
   end
 
